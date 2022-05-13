40d68a1c58ed347069917479cc40992eb3ecd861
NishanthMekala <104415101+NishanthMekala@users.noreply.github.com>
2022-05-13 13:05:09 +0530

    Add files via upload

---
+++ hw_sep_and_end.py
@@ -0,0 +1,9 @@
+'''sep=" "'''
+print('C','W','I',sep='')#CWI
+print('05','13','2022',sep='-')#05-13-2022
+
+'''end=""'''
+print('chess',end='.')
+print('com')#chess.com
+print('nishanth',end='@')
+print('gmail.com')#nishanth@gmail.com
